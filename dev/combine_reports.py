f42b4ae26dc6798da880d51568316350550961fb
Zachary Stephens <zach@zach-mbp-2019.local>
2020-03-02 00:43:01 -0600

    update

--- dev/combine_reports.py
+++ dev/combine_reports.py
@@ -736,7 +736,7 @@ if len(args.c) or len(COMPARE):
 	for n in COMPARE:
 		if len(COMPARE_OUT[n]) == 0:
 			anno = [isInBadRange(n[0],n[1])*'gap', BED_TRACKS[2][1].query(n[0],n[1])*'repeats', BED_TRACKS[3][1].query(n[0],n[1])*'mappability', BED_TRACKS[4][1].query(n[0],n[1])*'exclude']
-			print('MISS\t' + ', '.join([m for m in anno if len(m)]))
+			print('MISS\t' + str(n) + '\t' + ', '.join([m for m in anno if len(m)]))
 		else:
 			s1 = ' '.join([str(m) for m in COMPARE_OUT[n][0][0:5]])
 			s2 = str(n)
