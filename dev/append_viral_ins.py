2f87bbfc3cdddfb40f2756470a72e124963f6928
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 12:33:34 -0500

    2-->3

--- dev/append_viral_ins.py
+++ dev/append_viral_ins.py
@@ -1,5 +1,4 @@
 import sys
-import os
 
 #python append_viral_ins.py pbsv_ins_virus.sam duster.retain Viral_Junctions_LongReads.tsv Viral_Ins_LongReads.tsv [hifi/clr]
 
@@ -16,60 +15,60 @@ MODE    = sys.argv[5]
 BUFFER  = 100
 
 if MODE == 'hifi':
-	read_suffix = '/ccs'
+    read_suffix = '/ccs'
 elif MODE == 'clr':
-	read_suffix = '/clr'
+    read_suffix = '/clr'
 else:
-	print 'Unknown mode.'
-	exit(1)
+    print('Unknown mode.')
+    exit(1)
 
 existing_junctions = []
 f = open(IN_TSV, 'r')
 for line in f:
-	splt = line.strip().split('\t')
-	splt2 = splt[1].split(':')
-	existing_junctions.append((splt2[0], int(splt2[1])))
+    splt = line.strip().split('\t')
+    splt2 = splt[1].split(':')
+    existing_junctions.append((splt2[0], int(splt2[1])))
 f.close()
 
 read_whitelist = {}
 f = open(IN_DUST, 'r')
 for line in f:
-	read_whitelist[line.strip()] = True
+    read_whitelist[line.strip()] = True
 f.close()
 
 out_dat = []
 f = open(IN_SAM, 'r')
 for line in f:
-	splt = line.strip().split('\t')
-	flag = int(splt[1])
-	ref  = splt[2]
-	if not(flag&2048) and ref[:5] == 'virus':		# only use primary alignments to virus
-		if splt[0] in read_whitelist:				# and we passed complexity filters
-			splt2 = splt[0].split('_')
-			r_ref = splt2[2]
-			if r_ref not in HUMAN_CHR:				# don't bother with virus-virus SVs
-				continue
-			r_pos = int(splt2[3])+1
-			any_hit = False
-			for n in existing_junctions:
-				if n[0] == r_ref and abs(r_pos - n[1]) <= BUFFER:
-					any_hit = True
-					break
-			if any_hit == False:
-				ref_len    = str(100)			# pbsv default threshold
-				ref_mapq   = str(20)			# pbsv default threshold
-				viral_len  = str(len(splt[9]))
-				viral_mapq = splt[4]
-				qc_gap     = str(0)
-				out_dat.append(['ins_'+str(len(out_dat)+1)+read_suffix,
-					            r_ref+':'+str(r_pos),
-					            splt[2]+':'+splt[3],
-					            ref_len+','+ref_mapq,
-					            viral_len+','+viral_mapq,
-					            qc_gap])
+    splt = line.strip().split('\t')
+    flag = int(splt[1])
+    ref  = splt[2]
+    if not(flag & 2048) and ref[:5] == 'virus':     # only use primary alignments to virus
+        if splt[0] in read_whitelist:               # and we passed complexity filters
+            splt2 = splt[0].split('_')
+            r_ref = splt2[2]
+            if r_ref not in HUMAN_CHR:              # don't bother with virus-virus SVs
+                continue
+            r_pos = int(splt2[3])+1
+            any_hit = False
+            for n in existing_junctions:
+                if n[0] == r_ref and abs(r_pos - n[1]) <= BUFFER:
+                    any_hit = True
+                    break
+            if any_hit is False:
+                ref_len    = str(100)           # pbsv default threshold
+                ref_mapq   = str(20)            # pbsv default threshold
+                viral_len  = str(len(splt[9]))
+                viral_mapq = splt[4]
+                qc_gap     = str(0)
+                out_dat.append(['ins_'+str(len(out_dat)+1)+read_suffix,
+                                r_ref+':'+str(r_pos),
+                                splt[2]+':'+splt[3],
+                                ref_len+','+ref_mapq,
+                                viral_len+','+viral_mapq,
+                                qc_gap])
 f.close()
 
 f = open(OUT_TSV, 'w')
 for n in out_dat:
-	f.write('\t'.join(n)+'\n')
+    f.write('\t'.join(n)+'\n')
 f.close()
--- dev/duster_filter.py
+++ dev/duster_filter.py
@@ -2,47 +2,48 @@ import sys
 
 # dustmasker -in reads.fa -outfmt fasta | python duster_filter.py max_lowcomplex_frac duster.out duster.retain duster.remove
 
-THRESH   = float(sys.argv[1])/100.	# input is given as integer percent, e.g. "70"
+THRESH   = float(sys.argv[1])/100.  # input is given as integer percent, e.g. "70"
 F_OUT    = sys.argv[2]
 F_RETAIN = sys.argv[3]
 F_REMOVE = sys.argv[4]
 
 if not sys.stdin.isatty():
-	input_stream = sys.stdin
+    input_stream = sys.stdin
 else:
-	print 'No input.'
-	exit(1)
+    print('No input.')
+    exit(1)
 
 f1 = open(F_OUT,'w')
 f2 = open(F_RETAIN,'w')
 f3 = open(F_REMOVE,'w')
 current_readname = ''
 total_len        = 0
+low_complex      = 0
 for line in input_stream:
-	if line[0] == '>':
-		if current_readname != '':
-			f1.write(str(current_readname)+'\t'+str(low_complex)+'\t'+str(total_len)+'\n')
-			if float(low_complex)/float(total_len) <= THRESH:
-				f2.write(current_readname+'\n')
-			else:
-				f3.write(current_readname+'\n')
-		current_readname = line.strip()[1:]
-		low_complex      = 0
-		total_len        = 0
-	else:
-		rDat = line.strip()
-		low_complex += rDat.count('a')
-		low_complex += rDat.count('c')
-		low_complex += rDat.count('g')
-		low_complex += rDat.count('t')
-		total_len   += len(rDat)
+    if line[0] == '>':
+        if current_readname != '':
+            f1.write(str(current_readname)+'\t'+str(low_complex)+'\t'+str(total_len)+'\n')
+            if float(low_complex)/float(total_len) <= THRESH:
+                f2.write(current_readname+'\n')
+            else:
+                f3.write(current_readname+'\n')
+        current_readname = line.strip()[1:]
+        low_complex      = 0
+        total_len        = 0
+    else:
+        rDat = line.strip()
+        low_complex += rDat.count('a')
+        low_complex += rDat.count('c')
+        low_complex += rDat.count('g')
+        low_complex += rDat.count('t')
+        total_len   += len(rDat)
 
 if total_len > 0:
-	f1.write(str(current_readname)+'\t'+str(low_complex)+'\t'+str(total_len)+'\n')
-	if float(low_complex)/float(total_len) <= THRESH:
-		f2.write(current_readname+'\n')
-	else:
-		f3.write(current_readname+'\n')
+    f1.write(str(current_readname)+'\t'+str(low_complex)+'\t'+str(total_len)+'\n')
+    if float(low_complex)/float(total_len) <= THRESH:
+        f2.write(current_readname+'\n')
+    else:
+        f3.write(current_readname+'\n')
 
 f3.close()
 f2.close()
